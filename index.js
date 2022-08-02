c97f130dbcbd741f3b8432804b7dd7064ea805bd
Hannah Christmann <hc109909@gmail.com>
2022-08-02 13:11:19 -0400

    commit tree a3db261197c35cce876a9af56a0067f6fb1ae946

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    });
+}
+
+const steve = document.getElementById('steve');
+
+function addMoreEventListeners() {
+    steve.addEventListener('click', function(event){
+        console.log('BOOOOOOOOOO');
+    });
 }
